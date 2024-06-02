e9f96add7e4fb422f43a3b4439866d6aa919d3c2
Xuebin Zhao <xuebinzhao.zxb@gmail.com>
2024-06-02 18:11:28 -0500

    Change activation function from Relu to tanh

--- geopvi/nfvi/utils.py
+++ geopvi/nfvi/utils.py
@@ -294,8 +294,8 @@ class MLP(nn.Module):
             self.network.extend([
                     nn.Linear(h0, h1),
                     # nn.BatchNorm1d(h1),
-                    # nn.Tanh(),
-                    nn.LeakyReLU(0.2),
+                    nn.Tanh(),
+                    # nn.LeakyReLU(0.2),
                 ])
         self.network.extend([nn.Linear(h1, out_dim)])
         self.network = nn.Sequential(*self.network)
